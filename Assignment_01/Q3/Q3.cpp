99df5baee14fcfd41c54a07fed39008c31cf4cf6
rainats <raina.thomas@smail.inf.h-brs.de>
2018-11-10 20:00:25 +0100

    Adding Q3

--- Assignment_01/Q3/Q3.cpp
+++ Assignment_01/Q3/Q3.cpp
@@ -1,102 +1,130 @@
 #include<iostream>
 #include<math.h>
+#include<stdio.h>
+#include<iomanip>
+#include<string>
+#include<climits>
+#include<cmath>
+
 using namespace std;
 
-void print_table();
-int* func_1(int , int );
-int* func_2(int , int );
-int* func_3(int , int );
-int* func_4(int , int );
-int* func_5(int , int );
-int* func_6(int , int );
+int* func_1();
+float* func_2();
+string * func_3();
+int* func_4();
+string * func_5();
+string * func_6();
+void print_table(int [] ,float [] ,string [] ,int [] ,string [] ,string []);
 
 
-int* func_1(int lower, int upper)
+int* func_1()
 {
   //f (n) = 2n
   static int arr[101] = {};
-  for (size_t i = 0 ;i <=100; i++)
+  for (int i = 0 ;i <=100; i++)
   {
     arr[i] = 2*i;
   }
-
   return arr;
 }
 
-int* func_2(int lower, int upper)
+float* func_2()
 {
   //f (n) = sqrt(n)
-  static int arr[101] = {};
-  for (size_t i = 0 ;i <=100; i++)
+  static float arr[101] = {};
+  for (int i = 0 ;i <=100; i++)
   {
     arr[i] = sqrt(i);
   }
-
   return arr;
 }
 
-int* func_3(int lower, int upper)
+string* func_3()
 {
   //f (n) = 10^n
-  static int arr[101] = {};
-  for (size_t i = 0 ;i <=100; i++)
+  static long long int temp = 0;
+  static string arr_str[101] = {};
+  for (int i = 0 ;i <=100; i++)
   {
-    arr[i] = pow(10,i);
+    temp = pow(10,i);
+    if (abs(temp+1) >= LLONG_MAX)
+    {
+      arr_str[i] = "10^"+to_string(i);
+    }
+    else
+    {
+      arr_str[i] = to_string(temp);
+    }
   }
-
-  return arr;
+  return arr_str;
 }
 
-int* func_4(int lower, int upper)
+int* func_4()
 {
   //f (n) = n^3
   static int arr[101] = {};
-  for (size_t i = 0 ;i <=100; i++)
+  for (int i = 0 ;i <=100; i++)
   {
     arr[i] = pow(i,3);
   }
-
   return arr;
 }
 
-int* func_5(int lower, int upper)
+string * func_5()
 {
   //f (n) = 2^(1/n)
-  static int arr[101] = {};
-  for (size_t i = 0 ;i <=100; i++)
+  static string arr[101] = {};
+  arr[0] = "Inf";
+  for (int i = 1 ;i <=100; i++)
   {
-    arr[i] = pow(2, 1/i);
+    arr[i] = to_string(pow(2, 1/i));
   }
-
   return arr;
 }
 
-int* func_6(int lower, int upper)
+string * func_6()
 {
   //f (n) = e^(n)
-  static int arr[101] = {};
-  for (size_t i = 0 ;i <=100; i++)
+  static long long int temp = 0;
+  static string arr_str[101] = {};
+  for (int i = 0 ;i <=100; i++)
   {
-    arr[i] = exp(i);
+    temp = exp(i);
+    if (abs(temp+1) >= LLONG_MAX)
+    {
+      arr_str[i] = "e^("+to_string(i)+")";
+    }
+    else
+    {
+      arr_str[i] = to_string(temp);
+    }
   }
-
-  return arr;
+  return arr_str;
 }
 
-void print_table()
+void print_table(int arr_1[],float arr_2[],string arr_3[],int arr_4[],string arr_5[],string arr_6[])
 {
-  func_1(lower, upper);
-  func_2(lower, upper);
-  func_3(lower, upper);
-  func_4(lower, upper);
-  func_5(lower, upper);
-  func_6(lower, upper);  
+  // printing the table
+  cout << endl;
+  for (int i = 0; i < 183; i++) {cout << "=";}
+  cout << endl;
+  printf("|%25s|%25s|%25s|%25s|%25s|%25s|%25s|\n", "Number", "2*n", "sqrt(n)", "10^(n)", "n^(3)", "2^(1/n)", "e^(n)");
+  for (int i = 0; i < 183; i++) {cout << "=";}
+  cout << endl;
+  for (int i = 0; i < 101 ; i ++)
+  {
+    cout << setw(25) <<i << setw(25) << arr_1[i] << setw(28) << arr_2[i] << setw(27) << arr_3[i] << setw(25) << arr_4[i] << setw(25) << arr_5[i] << setw(25) << arr_6[i]<<"\n";
+  }
 }
 
 int main()
 {
-  int lower = 0;
-  int upper = 100;
-  print_table(lower,upper);
+  int * arr_1 = func_1();
+  float * arr_2 = func_2();
+  string * arr_3 = func_3();
+  int * arr_4 = func_4();
+  string * arr_5 = func_5();
+  string * arr_6 = func_6();
+  print_table(arr_1,arr_2,arr_3,arr_4,arr_5,arr_6);
   return 0;
 }
