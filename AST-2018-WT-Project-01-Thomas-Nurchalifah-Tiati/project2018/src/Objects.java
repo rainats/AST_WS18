e28b6ea049869489d692318878d7d9859f41db62
Desi <desiana.nurchalifah@smail.inf.h-brs.de>
2018-11-30 17:18:37 +0100

    Adding working folder

---
+++ AST-2018-WT-Project-01-Thomas-Nurchalifah-Tiati/project2018/src/Objects.java
@@ -0,0 +1,16 @@
+
+public class Objects {
+	
+	public String ObjectName;
+	public int ObjectNumber;
+	public String ObjectPercentage;
+	
+	public Objects(String ObjectName , int ObjectNumber , String ObjectPercentage ){
+		
+		this.ObjectName			= ObjectName ;
+		this.ObjectNumber		= ObjectNumber ;
+		this.ObjectPercentage	= ObjectPercentage ;
+
+}
+}
+	
---
+++ AST-2018-WT-Project-01-Thomas-Nurchalifah-Tiati/project2018/src/ProjectPart01.java
@@ -0,0 +1,10 @@
+
+
+public class ProjectPart01 {
+	
+	public static void main(String[] args){
+		
+			}
+
+}
+
