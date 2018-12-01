ac219af8fb0a475d717c0510fc39626810fd6fac
rainats <raina.thomas@smail.inf.h-brs.de>
2018-12-01 12:38:14 +0100

    Implementation of Objects class

--- AST-2018-WT-Project-01-Thomas-Nurchalifah-Tiati/project2018/src/Objects.java
+++ AST-2018-WT-Project-01-Thomas-Nurchalifah-Tiati/project2018/src/Objects.java
@@ -1,18 +1,28 @@
 package Project;
 
 public class Objects {
- 
+    
+    private String ObjectName;
+    private int ObjectNumber;
+    private String ObjectPercentage;
     
     public Objects(String ObjectName , int ObjectNumber , String ObjectPercentage ){
-        }
+        
+        this.ObjectName            = ObjectName ;
+        this.ObjectNumber        = ObjectNumber ;
+        this.ObjectPercentage    = ObjectPercentage ;}
 
 	public Object getObjectNumber() {
+		
+		return ObjectNumber ;
 	}
 
 	public String getObjectPercentage() {
-	
+		
+		return ObjectPercentage;
 	}
 	public Object getObjectName() {
-
+		
+		return ObjectName ;
 	}
 }
