581e96b3919ae86b2131b21f6f98dbcdf9a48a24
Desi <desiana.nurchalifah@smail.inf.h-brs.de>
2018-12-01 12:33:32 +0100

    Changing code format

--- AST-2018-WT-Project-01-Thomas-Nurchalifah-Tiati/project2018/src/Objects.java
+++ AST-2018-WT-Project-01-Thomas-Nurchalifah-Tiati/project2018/src/Objects.java
@@ -1,7 +1,18 @@
+package Project;
 
 public class Objects {
-	public Objects(){
+ 
+    
+    public Objects(String ObjectName , int ObjectNumber , String ObjectPercentage ){
+        }
 
-}
-}
+	public Object getObjectNumber() {
+	}
+
+	public String getObjectPercentage() {
 	
+	}
+	public Object getObjectName() {
+
+	}
+}
--- AST-2018-WT-Project-01-Thomas-Nurchalifah-Tiati/project2018/src/ProjectPart01.java
+++ AST-2018-WT-Project-01-Thomas-Nurchalifah-Tiati/project2018/src/ProjectPart01.java
@@ -1,10 +1,13 @@
-
-
+package Project;
 public class ProjectPart01 {
-	
-	public static void main(String[] args){
-		
-	}
+    public Objects[] ppart01(Objects[] array1,Objects[] array2) {
 
+    }
+public static void main(String[] args){
+        
+
+        
+    }
+        
+        
 }
-
